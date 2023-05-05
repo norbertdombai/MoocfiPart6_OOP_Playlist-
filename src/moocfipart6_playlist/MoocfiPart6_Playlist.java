c72aab4d02b1dd04de81df0a6a1df86463c1ce36
Dombai Norbert <dombai.norbert@omikron.hu>
2023-05-05 15:18:23 +0200

    Practicing object-oriented programming

---
+++ src/moocfipart6_playlist/MoocfiPart6_Playlist.java
@@ -0,0 +1,20 @@
+
+package moocfipart6_playlist;
+
+/**
+ *
+ * @author dombai.norbert
+ */
+public class MoocfiPart6_Playlist {
+
+    public static void main(String[] args) {
+      
+        Playlist list = new Playlist();
+        list.addSong("Moment - Nicolas Binder");
+        list.addSong("Invisible - Paul Kalkbrenner Remix");
+        list.printSongs();
+    
+        
+    }
+    
+}
